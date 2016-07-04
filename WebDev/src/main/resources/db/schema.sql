6b154a1138c776ebc6990f763a6a1b62b428ef02
Mallikanand <anand.kotra@gmail.com>
2016-07-04 07:57:25 +0100

    Tables and their relationships

--- WebDev/src/main/resources/db/schema.sql
+++ WebDev/src/main/resources/db/schema.sql
@@ -1,3 +1,21 @@
+/** 
+ * Tables
+ * ------
+ * MenuItem:item_name(PK),menu_type, food_type, price, inactive
+ * Order: id(PK), total_value, order_date, delivery_date, delivery_address_id(FK), user_id(FK), status
+ * User:user_id(PK), registered_address_id(FK), password, email, active, creation_date, 
+ * Address: id(PK), address_id, added_date, verified
+ * 
+ * mapping tables
+ * --------------
+ * Order_Item: order_id(PK),item_name(PK), price, quantity
+ * 
+ * Relationships
+ * -------------
+ * Order:MenuItem - N:N
+ * User:Order - 1:N
+ * 
+ */
 create table MenuItem (
 	item_name varchar(50) not null, 
 	menu_type varchar(15) not null,-- Indian, Italian, Continental,.. others
@@ -6,3 +24,4 @@ create table MenuItem (
 	inactive varchar(1) not null,
 	Primary Key (item_name)
 );
+
