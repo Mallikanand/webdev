251b7afc11f9d64c81cd81424da5938188ac00f9
Mallikanand <anand.kotra@gmail.com>
2016-07-11 07:39:14 +0100

    HomeController Test

---
+++ WebDev/src/main/webapp/com/webdev/config/MvcConfiguration.java
@@ -0,0 +1,22 @@
+package com.webdev.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public ViewResolver getViewResolver(){
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/");
+		viewResolver.setSuffix(".jspx");
+		return viewResolver; 
+	}
+}
--- WebDev/src/main/webapp/com/webdev/controllers/HomeController.java
+++ WebDev/src/main/webapp/com/webdev/controllers/HomeController.java
@@ -1,13 +1,39 @@
 package com.webdev.controllers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.webdev.data.model.MenuItem;
+import com.webdev.data.model.MenuType;
+import com.webdev.services.MenuService;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private	MenuService menuService; 
+
 	@RequestMapping(value="/")
-	public String home(){
-		return "welcome";
+	public ModelAndView home(){
+
+		Map<MenuType, List<MenuItem>> menuItemsByMenuType = menuService.getMenuItemsByMenuType();
+		
+		Map<String,Map<MenuType, List<MenuItem>>> model = new HashMap<>();
+		model.put("menuItemsByMenuType", menuItemsByMenuType);
+		
+		return new ModelAndView("welcome",model);
+	}
+
+	public MenuService getMenuService() {
+		return menuService;
+	}
+	public void setMenuService(MenuService menuService) {
+		this.menuService = menuService;
 	}
 }
